d4faa82ff98dfaec662cdf5aca6cb2850b3557ce
Bushido Gino <bushidog@student.21-school.ru>
2023-11-01 17:49:05 +0000

    мама мне плохо

---
+++ src/s21_stack.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "s21_list.h"
+
+namespace s21 {
+    template<typename T>
+    class stack {
+    private:
+        list<T> data;
+    public:
+        using value_type = T;
+        using reference = T&;
+        using const_reference = const T&;
+        using size_type = size_t;
+
+        ////////////////////////////////////////
+        // CONSTRUCTORS
+        ////////////////////////////////////////
+
+        stack() {}
+        stack(std::initializer_list<T> values) : data(values.begin(), values.end()) {}
+        stack(const stack<T>& other) : data(other.data) {}
+        stack(stack<T>&& other) : data(std::move(other.data)) {}
+        operator=(stack&& other) {return *this = other;}
+        ~stack() {data.clear();}
+
+        ////////////////////////////////////////
+        // List Element access
+        ////////////////////////////////////////
+
+        const_reference top() const {return data.back();}
+
+        ////////////////////////////////////////
+        // List Capacity
+        ////////////////////////////////////////
+
+        bool empty() const {return data.empty();}
+        size_type size() const {return data.getSize();}
+
+        ////////////////////////////////////////
+        // List Modifiers
+        ////////////////////////////////////////
+
+        void push(const T& value) {data.push_back(value);}
+        void pop() {data.pop_back();}
+        void swap(stack &other) { data.swap(other.data); }
+    };
+}
